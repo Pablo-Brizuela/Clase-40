895cf4025ce0c1e32ee0a55cba32873db6bd4c73
Pablo-Brizuela <pablo_2002_cab_@hotmail.com>
2022-11-03 15:41:59 -0300

    Editar y borrar falta crear

--- js/formulario.js
+++ js/formulario.js
@@ -5,46 +5,84 @@ window.onload = () => {
     const releaseDate = document.querySelector("#release_date");
     const length = document.querySelector("#length");
     const btnEditar = document.querySelector(".botonModificar");
-  
+    const btnEliminar = document.querySelector(".botonBorrar");
+
     const qs = new URLSearchParams(location.search);
   
-    const id = qs.get("peliculaId");
+    let id = null
   
-    fetch("http://localhost:3031/api/movies/" + id)
-      .then((respuesta) => {
-        return respuesta.json();
-      })
-      .then((pelicula) => {
-        console.log(pelicula);
-      });
+      if(qs.has("peliculaId")){
+        id = qs.get("peliculaId");
+        fetch("http://localhost:3031/api/movies/" + id)
+        .then((respuesta) => {
+          return respuesta.json();
+        })
+        .then((pelicula) => {
+          console.log(pelicula);
+          title.value = pelicula.data.title
+          rating.value = pelicula.data.rating
+          awards.value = pelicula.data.awards
+          releaseDate.value = new Date(pelicula.data.release_date)
+          .toISOString()
+          .split("T")[0]
+          length.value = pelicula.data.length
+        });
+      }
+
+    
+        
+     
   
     btnEditar.addEventListener("click", (e) => {
       e.preventDefault();
   
       const data = {
-        title: "",
-        rating: "",
-        awards: "",
-        release_date: "",
-        length: "",
-        genre_id: "",
+        title: title.value,
+        rating: rating.value,
+        awards: awards.value,
+        release_date: release_date.value,
+        length: length.value,
+        /* genre_id: "", */
       };
   
       const setting = {
-        method: "POST",
+        method: "PUT",
         headers: {
           "content-type": "application/json",
         },
         body: JSON.stringify(data),
       };
   
-      fetch("http://localhost:3031/api/movies/" + id, setting)
+      fetch("http://localhost:3031/api/movies/update/" + id, setting)
         .then((respuesta) => {
           return respuesta.json();
         })
         .then((data) => {
           console.log(data);
           location.href = "home.html";
+        });
+
+        btnEliminar.addEventListener("click", (e) => {
+          e.preventDefault();
+      
+        
+      
+          const setting = {
+            method: "DELETE",
+            headers: {
+              "content-type": "application/json",
+            },
+            
+          };
+      
+          fetch("http://localhost:3031/api/movies/delete/" + id)
+            .then((respuesta) => {
+              return respuesta.json();
+            })
+            .then((data) => {
+              console.log(data);
+              location.href = "home.html";
+            });
         });
     });
   };
